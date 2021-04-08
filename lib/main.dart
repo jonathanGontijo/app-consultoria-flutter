39e7405fadac6096679ee81ea88c29d0cf6ea318
jonathanGontijo <jonathan.ferreira1312@hotmail.com>
2021-04-08 20:39:49 -0300

    App consultoria

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:consultoria/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home() ,
+    debugShowCheckedModeBanner: false,
+
+  ));
+}
+
